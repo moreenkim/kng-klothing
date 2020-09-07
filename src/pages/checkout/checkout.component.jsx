d09796cd78ed7feb221bb63dabc028ffb3b191ca
Moreen <moreenkim36@gmail.com>
2020-09-07 12:33:51 +0300

    Integrate the use of STRIPE payment method to our application

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import stripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import {
@@ -35,6 +35,11 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      **Please use the following test details for payment**
+      <br />
+      4242 4242 4242 4242 - Exp: 12/21 - CVV: 123
+    </div>
     <StripeCheckoutButton price={total} />
   </div>
 );
