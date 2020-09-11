caede1d1c02f78471cd4a781dd186355593a5bfc
Moreen <moreenkim36@gmail.com>
2020-09-11 09:39:15 +0300

    Add the redux-saga functionality

---
+++ src/redux/shop/shop.saga.js
@@ -0,0 +1,9 @@
+// import { takeEvery } from 'redux-saga/effects';
+
+// import ShopActionTypes from './shop.types';
+
+// export function* fetchCollectionsAsync() {}
+
+// export function* fetchCollectionsStart() {
+//   yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START);
+// }
